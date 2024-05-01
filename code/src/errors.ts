9f5fa1cb7a8128af47fc4e472e71b76894ec52f9
Lukáš Průša <lukyn.patai5@gmail.com>
2024-05-01 15:03:58 +0200

    fix: schema description error detection

--- code/src/errors.ts
+++ code/src/errors.ts
@@ -5,9 +5,10 @@ export const REPETITIVE_PROMPT_ERROR_MESSAGE = 'OpenAI has rejected a page promp
     + 'try to remove the repetitive content via the `removeElementsCssSelector` input option. Skipping GPT processing '
     + 'for this page!';
 
-export const DESCRIPTION_LENGTH_ERROR = "\" is too long - 'functions.0.description'";
+export const DESCRIPTION_LENGTH_ERROR = "is too long - 'functions.0.description'";
 export const DESCRIPTION_LENGTH_ERROR_LOG_MESSAGE = 'Schema function description is too long, please shorten it to a '
-    + 'maximum of 1280 characters.';
+    + 'maximum of 1280 characters.\n\t- TIP: You can use `instructions` separately from `schemaDescription` to '
+    + 'provide more context.';
 
 export const ERROR_OCCURRED_MESSAGE = "An error has occurred in your run. Please check the run's logs for more info.";
 
